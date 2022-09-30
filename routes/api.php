9ed416a5e373c809893685ae7e5e91280969a91d
TRUONGKHOA <phamtruongkhoa2000@gmail.com>
2022-10-01 01:21:22 +0700

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products', 'ProductController');
+
+Route::prefix('products')->group(function () {
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+});
