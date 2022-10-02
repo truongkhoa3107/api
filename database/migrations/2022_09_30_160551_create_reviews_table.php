64cad128329c4a8cac3ba47c32ed5ab26f207dd7
TRUONGKHOA <phamtruongkhoa2000@gmail.com>
2022-10-02 23:39:07 +0700

    Migration created for products and reviews

--- database/migrations/2022_09_30_160551_create_reviews_table.php
+++ database/migrations/2022_09_30_160551_create_reviews_table.php
@@ -14,8 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('product_id')->unsigned()->index();
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
